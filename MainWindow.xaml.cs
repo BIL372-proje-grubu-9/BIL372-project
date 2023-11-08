5b736012606917bba6073ba22ca24a61fd8261d1
Thnorty <ototatik@gmail.com>
2023-11-08 20:39:55 +0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string connectionString = "Server=localhost;Database=project;PASSWORD=1234;UID=root;";
+
+            MySqlConnection connection = new(connectionString);
+            connection.Open();
+
+            MySqlCommand studentsCmd = new("SELECT * FROM students", connection);
+            MySqlCommand employeesCmd = new("SELECT * FROM employees", connection);
+
+            DataTable studentsDataTable = new();
+            DataTable employeesDataTable = new();
+
+            studentsDataTable.Load(studentsCmd.ExecuteReader());
+            employeesDataTable.Load(employeesCmd.ExecuteReader());
+
+            StudentsGrid.ItemsSource = studentsDataTable.DefaultView;
+            EmployeesGrid.ItemsSource = employeesDataTable.DefaultView;
+
+            // Print the headers of the students table.
+            foreach (DataColumn column in studentsDataTable.Columns)
+            {
+                Console.Write($"{column.ColumnName} ");
+            }
+            Console.WriteLine();
+
+            connection.Close();
+        }
+
+        private void AddStudentButton_Click(object sender, RoutedEventArgs e)
+        {
+            StudentDialog studentDialog = new();
+            bool? result = studentDialog.ShowDialog();
+
+            if (result == true)
+            {
+                string studentName = studentDialog.StudentName;
+                string studentAge = studentDialog.StudentAge;
+
+                // Add the student to the database.
+                using MySqlConnection connection = new("Server=localhost;Database=project;PASSWORD=1234;UID=root;");
+                connection.Open();
+
+                string insertQuery = "INSERT INTO students (first_name, age) VALUES (@first_name, @age)";
+                MySqlCommand insertCommand = new(insertQuery, connection);
+                insertCommand.Parameters.AddWithValue("@first_name", studentName);
+                insertCommand.Parameters.AddWithValue("@age", studentAge);
+
+                int rowsAffected = insertCommand.ExecuteNonQuery();
+
+                if (rowsAffected > 0)
+                {
+                    // Refresh the students grid with the updated data.
+                    RefreshStudentsGrid();
+                }
+
+                connection.Close();
+            }
+        }
+
+        // Method to refresh the StudentsGrid with updated data from the database.
+        private void RefreshStudentsGrid()
+        {
+            using MySqlConnection connection = new("Server=localhost;Database=project;PASSWORD=1234;UID=root;");
+            connection.Open();
+
+            MySqlCommand studentsCmd = new("SELECT * FROM students", connection);
+            DataTable studentsDataTable = new();
+            studentsDataTable.Load(studentsCmd.ExecuteReader());
+
+            StudentsGrid.ItemsSource = studentsDataTable.DefaultView;
+
+            connection.Close();
+        }
+    }
+}
---
+++ StudentDialog.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for StudentDialog.xaml
+    /// </summary>
+    public partial class StudentDialog : Window
+    {
+        public string StudentName { get; set; }
+        public string StudentAge { get; set; }
+        public StudentDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Validate input (you can add more validation if needed).
+            if (string.IsNullOrWhiteSpace(StudentNameTextBox.Text))
+            {
+                MessageBox.Show("Please enter the student's name.");
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(StudentAgeTextBox.Text))
+            {
+                MessageBox.Show("Please enter the student's age.");
+                return;
+            }
+
+            // Set the StudentName and StudentAge property with the entered name.
+            StudentName = StudentNameTextBox.Text;
+            StudentAge = StudentAgeTextBox.Text;
+
+            // Close the dialog with a "true" result to indicate success.
+            DialogResult = true;
+        }
+        private void NumericOnly_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            // Use this event handler to allow only numeric input.
+            if (!int.TryParse(e.Text, out _))
+            {
+                e.Handled = true; // Cancel the input if it's not a valid integer.
+            }
+        }
+
+    }
+}
